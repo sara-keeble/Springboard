2ffdf23c3c784792fb722467348b8b484b615750
Sara Keeble <sara.keeble@thermofisher.com>
2021-03-22 08:29:36 -0700

    scrapy mini project added

---
+++ notebooks/scrapy_mini_project/scrapy_mini_project/spiders/toscrape-xpath_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "toscrape-xpath"
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('.//span[@class="text"]/text()').get(),
+                'author': quote.xpath('.//small[@class="author"]/text()').get(),
+            }
